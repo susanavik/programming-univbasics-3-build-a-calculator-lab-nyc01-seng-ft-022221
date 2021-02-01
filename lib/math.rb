9b71b1087e86a0635aa342d60fdc86e67da5872c
Susana vik <susana.m.vik@gmail.com>
2021-02-01 19:45:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,41 +1,46 @@
 def addition(num1, num2)
   sum = num1.to_i + num2.to_i
   puts "#{sum}"
-  return sum
 end
 
+num1 = '5'
+num2 = '4'
 addition(num1, num2)
 
 def subtraction(num1, num2)
   sum = num1.to_i - num2.to_i
   puts "#{sum}"
-  return sum
 end
 
+num1 = '10'
+num2 = '5'
 subtraction(num1, num2)
 
 def division(num1, num2)
   sum = num1.to_i / num2.to_i
   puts "#{sum}"
-  return sum
 end
 
+num1 = '50'
+num2 = '2'
 division(num1, num2)
 
 def multiplication(num1, num2)
   sum = num1.to_i * num2.to_i
   puts "#{sum}"
-  return sum
 end
 
+num1 = '4'
+num2 = '30'
 multiplication(num1, num2)
 
 def modulo(num1, num2)
   sum = num1.to_i % num2.to_i
   puts "#{sum}"
-  return sum
 end
 
+num1 = '34'
+num2 = '5'
 modulo(num1, num2)
 
 def square_root(num)
@@ -44,4 +49,5 @@ def square_root(num)
   return sum
 end
 
+num = '81'
 square_root(num)
