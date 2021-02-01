4db74fbfb4b228e8ad40a95442150d6d7c3722e6
Susana vik <susana.m.vik@gmail.com>
2021-02-01 19:32:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -17,12 +17,22 @@ num2 = '3'
 subtraction(num1, num2)
 
 def division(num1, num2)
-
+  sum = num1.to_i / num2.to_i
+  puts "#{sum}"
 end
+
+num1 = '3'
+num2 = '1'
+division(num1, num2)
 
 def multiplication(num1, num2)
-
+  sum = num1.to_i * num2.to_1
+  puts '#{sum}'
 end
+
+num1 = '5'
+num2 = '3'
+multiplication(num1, num2)
 
 def modulo(num1, num2)
 
